88f8ece3cc8f0d5045036e7ed2df926ce32ff915
erickcandro <ericksantiagocandrovalencia@gmail.com>
2023-04-26 19:17:00 -0500

    ejercicios comentariados

--- ciclos/for1.py
+++ ciclos/for1.py
@@ -9,3 +9,7 @@ for k in range (0,11,2):
 
 for i in range (10,-1,-2):
     print(i)
+
+    #este ejercicio se hizo a base de el comando for
+    #para mostrar el funcionamiento de el mismo aplicandolo en una situacion
+    
---
+++ ciclos/while3.py
@@ -0,0 +1,6 @@
+num=1
+cont=0
+while num!=0:
+  num=int(input('ingrese numero'))
+  cont=cont+1 
+
