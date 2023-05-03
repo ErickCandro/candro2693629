152bcc60d5a94edd39bdaf183d1cd103ad2c4471
erickcandro <ericksantiagocandrovalencia@gmail.com>
2023-05-03 11:40:41 -0500

    ejercicios con los ejemplos hechos con listas

---
+++ listas/list1.py
@@ -0,0 +1,24 @@
+import random
+
+suma=0
+max=0
+min=1000
+prom=0
+lista= []
+tam= int(random.randint (10,20))
+print(tam)
+for i in range (tam):
+    num= int(random.randrange(100))
+    lista.append(num)
+    suma+=num
+    prom=suma/tam
+    if num>max:
+        max=num
+    if num<min and num!=0:
+        min=num
+
+print ("la lista de numeros es ", lista)
+print ("la suma de los numeros es ",suma)
+print("el promedio es",prom)
+print ("el numero maximo es ",max)
+print ("el numero minimo es",min)
